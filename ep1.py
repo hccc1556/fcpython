50654b9893b1c70d26a78bf317679db3c8e6c538
hccc1556 <80061459+hccc1556@users.noreply.github.com>
2021-03-09 21:56:59 +0800

    Add files via upload

---
+++ ep1.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+for i in range(1,10,2):
+    print(i,end=',')
+    
+print()
+
+for i in range(10,0,-1):
+    print(i,end=',')
